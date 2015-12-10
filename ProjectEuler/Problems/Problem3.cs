fa4e592cbf6ff6fe46ff27c4f490f5078d176b00
Evan Frisch <walkerboh1213@gmail.com>
2015-12-09 19:09:13 -0500

    Problem 3 complete

---
+++ ProjectEuler/Problems/Problem3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem3 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            long largestPrimeFactor = Convert.ToInt64(Math.Sqrt(600851475143)) + 1;
+            bool done = false;
+
+            while (largestPrimeFactor > 3 && !done)
+            {
+                if (600851475143 % largestPrimeFactor != 0l)
+                {
+                    largestPrimeFactor -= 2;
+                    continue;
+                }
+
+                done = IsPrime(largestPrimeFactor);
+                if (!done)
+                    largestPrimeFactor -= 2;
+
+                Console.WriteLine(largestPrimeFactor);
+            }
+
+            Console.WriteLine("Problem 3 - Success: " + done);
+            Console.WriteLine("\tValue: " + largestPrimeFactor);
+        }
+
+        private bool IsPrime(long number)
+        {
+            long max = Convert.ToInt64(Math.Ceiling(Math.Sqrt(number)));
+
+            if (number == 2 || number == 3)
+                return true;
+
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            int n = 5;
+
+            while (n <= max)
+            {
+                if ((number % n == 0) || (number % (n + 2) == 0))
+                    return false;
+                n += 6;
+            }
+
+            return true;
+        }
+    }
+}
