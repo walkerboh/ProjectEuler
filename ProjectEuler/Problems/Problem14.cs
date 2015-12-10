f05ee780289feac7f123e4af1d7dadb77e29d272
Evan Frisch <walkerboh1213@gmail.com>
2015-12-09 21:53:28 -0500

    Problem 14 complete

---
+++ ProjectEuler/Problems/Problem14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem14 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            int longestChainNumber = 1, longestChainAmount = 1;
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                long num = i;
+                int chainLength = 1;
+
+                // Keep up the Collatz sequence until we get the number one
+                while (num != 1)
+                {
+                    if (num % 2 == 0)
+                        num = num / 2;
+                    else
+                        num = 3 * num + 1;
+
+                    chainLength++;
+                }
+
+                if (chainLength> longestChainAmount)
+                {
+                    longestChainAmount = chainLength;
+                    longestChainNumber = i;
+                }
+            }
+
+            Console.WriteLine("Problem 14: " + longestChainNumber);
+        }
+    }
+}
