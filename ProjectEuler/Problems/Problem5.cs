fec155785b7e0a10142fce7bd2ddd006d9833a06
Evan Frisch <walkerboh1213@gmail.com>
2015-12-09 20:07:56 -0500

    Problem 6 complete

--- ProjectEuler/Problems/Problem5.cs
+++ ProjectEuler/Problems/Problem5.cs
@@ -13,7 +13,7 @@ namespace ProjectEuler.Problems
             int number = 0;
             bool done = false;
 
-            // Super brute force but with some tricks to make it quicker
+            // Super brute force but with some tricks to make it quicker. Could have gone the LCM and GCD method, but this was quicker to type up with no built-ins (that I know of)
             while (!done)
             {
                 // I chose 180 because I knew this would handle most of the numbers smaller than 20 because of its factors, leaving me with only 7 numbers to check.
@@ -25,6 +25,7 @@ namespace ProjectEuler.Problems
             }
 
             Console.WriteLine("Problem 5: " + number);
+
         }
     }
 }
---
+++ ProjectEuler/Problems/Problem6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem6 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            double sumOfSquares = 0;
+            double squareOfSums = 0;
+
+            // Sum the squares for the first, make the sum for the second
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += Math.Pow(i, 2);
+                squareOfSums += i;
+            }
+
+            // Square the sum
+            squareOfSums = Math.Pow(squareOfSums, 2);
+
+            // Subtract!
+            Console.WriteLine("Problem 6: " + (squareOfSums - sumOfSquares));
+        }
+    }
+}
