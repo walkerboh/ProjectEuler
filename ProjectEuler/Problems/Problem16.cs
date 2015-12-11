d3952b4bb6befc4e6c9c5f68b282d509b6e1fda6
Evan Frisch <walkerboh1213@gmail.com>
2015-12-10 20:04:04 -0500

    Problem 16 complete

---
+++ ProjectEuler/Problems/Problem16.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem16 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            BigInteger product = new BigInteger(1);
+
+            // Calculate 2^1000
+            for (int i = 0; i < 1000; i++)
+                product *= 2;
+
+            // Get all the digits
+            char[] digits = product.ToString().ToCharArray();
+
+            long sum = 0;
+
+            // Convert each digit to a numeric and sum
+            foreach (char digit in digits)
+            {
+                sum += long.Parse(digit.ToString());
+            }
+
+            Console.WriteLine("Problem 16: " + sum);
+        }
+    }
+}
