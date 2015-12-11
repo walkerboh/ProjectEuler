2fb3f132841767eed068c055301f933db7c24097
Evan Frisch <walkerboh1213@gmail.com>
2015-12-10 21:48:48 -0500

    Problem 19 complete

---
+++ ProjectEuler/Problems/Problem19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem19 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            int total = 0;
+
+            DateTime dateTime = new DateTime(1901, 1, 1);
+
+            // Some may call this cheating but it is within the bounds of the challenge.
+            // You could use the average day distribution of days between the dates divided by 7 for days of the week and 30 for days in the month. It gets you close.
+            // Or you could calculate the days in each month in each year and one by one check if that loops around to a sunday with % but this is easier.
+            while (dateTime <= new DateTime(2000, 12, 31))
+            {
+                if (dateTime.DayOfWeek == DayOfWeek.Sunday)
+                    total++;
+
+                dateTime = dateTime.AddMonths(1);
+            }
+
+            Console.WriteLine("Problem 19: " + total);
+        }
+    }
+}
