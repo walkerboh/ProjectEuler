9172da39983791efd49e2ed7536aa2105fd028d6
Evan Frisch <walkerboh1213@gmail.com>
2015-12-13 10:40:58 -0500

    Problem 29 complete

---
+++ ProjectEuler/Problems/Problem29.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem29 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            List<BigInteger> values = new List<BigInteger>();
+
+            // Simple enough, just build a list of all possible combinations
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    values.Add(BigInteger.Pow(i, j));
+                }
+            }
+
+            // Get the distinct values
+            values = values.Distinct().ToList();
+
+            Console.WriteLine("Problem 29: " + values.Count);
+        }
+    }
+}
