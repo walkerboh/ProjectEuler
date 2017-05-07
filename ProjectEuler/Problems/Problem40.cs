2e29be91d0916351d330d694b740ed82708bd410
Evan Frisch <walkerboh1213@gmail.com>
2017-05-06 23:53:36 -0400

    Problem 40 solved

---
+++ ProjectEuler/Problems/Problem40.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem40 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            // There is probably a nice easy to find pattern here, but honestly, brute forcing it was too easy
+            
+            StringBuilder dec = new StringBuilder();
+
+            int cur = 1;
+
+            while (dec.Length < 1000000)
+            {
+                dec.Append(cur++);
+            }
+
+            long product = Convert.ToInt32(dec[0].ToString());
+            product *= Convert.ToInt32(dec[9].ToString());
+            product *= Convert.ToInt32(dec[99].ToString());
+            product *= Convert.ToInt32(dec[999].ToString());
+            product *= Convert.ToInt32(dec[9999].ToString());
+            product *= Convert.ToInt32(dec[99999].ToString());
+            product *= Convert.ToInt32(dec[999999].ToString());
+
+            Console.WriteLine("Problem 40: " + product);
+        }
+    }
+}
