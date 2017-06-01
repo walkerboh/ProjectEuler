5f90095b0dcf9a6bddd3201d536289ff3fb72e55
Evan Frisch <walkerboh1213@gmail.com>
2017-05-31 21:49:22 -0400

    Problem 48 solved

---
+++ ProjectEuler/Problems/Problem48.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem48 : BaseProblem
+    {
+        protected override void ExecuteProblem()
+        {
+            // I know there is some fancy modulo math I could, but this is really fast. Why bother...
+            BigInteger bigInt = new BigInteger();
+            bigInt = 0;
+
+            for (int i = 1; i <= 1000; i++)
+                bigInt += BigInteger.Pow(i, i);
+
+            Console.WriteLine("Problem 48: " + bigInt.ToString().Substring(bigInt.ToString().Length - 10));
+        }
+    }
+}
